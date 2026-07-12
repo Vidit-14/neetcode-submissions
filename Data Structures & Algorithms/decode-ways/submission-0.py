c2ab2f13d81033e112de53402b9c48a48ac2492c
Vidit-14 <viditrane006@gmail.com>
2026-07-13 00:55:33 +0530

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+
+        if s[0] == '0':
+            dp[1] = 0
+        else:
+            dp[1] = 1
+        
+        for i in range(2, n+1):
+            oneDigit = int(s[i-1 : i])
+            twoDigit = int(s[i-2 : i])
+
+            if oneDigit >= 1:
+                dp[i] += dp[i-1]
+            
+            if twoDigit >= 10 and twoDigit <= 26:
+                dp[i] += dp[i-2]
+        
+        return dp[n]
